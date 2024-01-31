c626e83d2b1560782eb7f48613626cf7bf804ad9
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-01-31 10:22:35 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild                          } from '@angular/core';
+import { NgxSignatureOptions, NgxSignaturePadComponent } from '@eve-sama/ngx-signature-pad';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //
   title = 'PWA_DEMO_ENV_TEST';
+   /** Catch object, call functions via instance object */
+   @ViewChild('signature') signature: NgxSignaturePadComponent | undefined;
+
+   /** You can see more introduction in the below about NgxSignatureOptions */
+   public options: NgxSignatureOptions = {
+     backgroundColor: '#F4F5F5',
+     width : 100,
+     height: 100,
+     css: {
+       'border-radius': '16px'
+     }
+   };
+ 
+   /** The begin event of sign */
+   onBeginSign(): void { }
+ 
+   /** The end event of sign */
+   onEndSign(): void { }
+   //
+   saveSignature() {
+      //
+      console.log("Saving signature...");
+      // PNG
+      let dataUrl : string  = this.signature?.toDataURL()!;
+      console.log('Data URL:', dataUrl);
+
+      // Create an anchor element
+      const downloadLink : HTMLAnchorElement = document.createElement('a');
+
+      // Set href attribute with the data URL
+      downloadLink.href = dataUrl;
+
+      // Set a suggested filename for the download
+      downloadLink.download = 'signature.png';
+
+      // Append the anchor to the body
+      document.body.appendChild(downloadLink);
+
+      // Trigger a click event on the anchor
+      downloadLink.click();
+
+      // Remove the anchor from the body
+      document.body.removeChild(downloadLink);
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxSignaturePadModule } from '@eve-sama/ngx-signature-pad';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgxSignaturePadModule,
     AppRoutingModule
   ],
   providers: [],
