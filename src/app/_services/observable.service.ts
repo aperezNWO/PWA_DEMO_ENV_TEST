9fbfc1096b04a8aa446e839472ef91a8979ac8d4
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-08-26 21:56:15 -0500

    [PWA DEMO] -- [ENV_TEST] v[1.3.3] 1) RxJs Playground.

--- src/app/_services/observable.service.ts
+++ src/app/_services/observable.service.ts
@@ -1,119 +1,110 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, concatMap, delay, from, interval, map, Observable, of, scan, Subject, take, tap } from 'rxjs';
+//  TEST BASIC OBSERVABLE
+let numbers$ = of(1, 2, 3,4,5);
+let _currentValue: any[] = [];
+let _delayedObservable: any;
+// TEST BEHAVIOUR SUBJECT
+let items         = ['a', 'b', 'c', 'd', 'e'];
+let itemSubject   = new BehaviorSubject<string | null>(null);
+let currentItem$  = itemSubject.asObservable();
+// TEST HANOI
+let _delayedObservableHanoi: any;
+//
+function TestBehaviourSubject() {
 
+  //
+  interval(1000)
+    .pipe(
+      take(items.length),
+      map((index: number) => items[index])
+    )
+    .subscribe({
+      next(item) { 
+
+          console.log("Behaviour subject item : " + item); 
+          
+          itemSubject.next(item)
+      },
+      error(err) { console.error('Behaviour subject erro: ' + err); },
+      complete() {
+        //  
+        console.log('Behaviour subject complete notification');
+        //
+        TestHanoi();
+      }
+    })
+}
+//
+function TestHanoi() {
+  //
+  const values = ['A', 'B', 'C', 'D', 'E'];
+  const delayTime = 1000; // Delay in milliseconds
+  //
+  _delayedObservableHanoi = from(values).pipe(
+    concatMap(value => of(value).pipe(delay(delayTime)))
+  );
+ //
+  _delayedObservableHanoi.subscribe({
+      next(result: any) { console.log(`Hanoi emitted Result: ${result}`); },
+      error(err: string) { console.error('Hanoi emitted an error: ' + err); },
+      complete() {
+        //  
+        console.log('Hanoi complete notification');
+      }
+    });
+}
 //
 @Injectable({
   providedIn: 'root'
 })
 export class ObservableService {
-
-  // --------------------------------------------------------
-  // BASIC OBSERVABLE
-  // --------------------------------------------------------
-  private _currentValue : any[] = [];
   //
-  public numbers$ = of(1, 2, 3);
-  //
-  public delayedObservable : any;
-
-  // --------------------------------------------------------
-  // BEHAVIOUR SUBJECT
-  // --------------------------------------------------------
-  private items        = ['a', 'b', 'c', 'd', 'e'];
-  private itemSubject = new BehaviorSubject<string | null>(null);
-  private currentItem$ = this.itemSubject.asObservable();
-  //
-  public constructor()
-  {
-      //
-      this.TestBasicObservable();
-      //
-      this.TestBehaviourSubject();
-      //
-      this.TestHanoi();
+  public constructor() {
+    //
+    this.TestBasicObservable();
   }
   //
-  TestBasicObservable()
-  {
-      //
-      this.delayedObservable = from(this.numbers$).pipe(
-        concatMap(value => of(value).pipe(delay(1000)))
-      );
-      //
-      this.delayedObservable.subscribe((value: number) => {
+  TestBasicObservable() {
+    //
+    _delayedObservable = from(numbers$).pipe(
+      concatMap(value => of(value).pipe(delay(1000)))
+    );
+    //
+    _delayedObservable.subscribe({
+      next(value: number) {
+        //
         console.log('Observable emitted the next value from service to array : ' + value);
-        this._currentValue.push(value);
-      });
-  }
-   //
-   TestBehaviourSubject()
-   { //
-      interval(1000)
-      .pipe(
-        take(this.items.length),
-        map((index: number) => this.items[index])
-      )
-      .subscribe((item) => {
-          
-          console.log("Behaviour subject item : " + item);
-
-          this.itemSubject.next(item)
-
-      });
+        _currentValue.push(value);
+      },
+      error(err: string) { console.error('Behaviour subject erro: ' + err); },
+      complete() {
+        //
+        console.log('Observable complete notification');
+        //
+        TestBehaviourSubject();
+      }
+    });
   }
   //
-  TestHanoi()
-  {
-      /*
-      //
-      let rods$: BehaviorSubject<number[][]> | undefined;
-      let moves$: Observable<number[][]> | undefined;
+  public get CurrentValue() {
+    return _currentValue
+  }
 
-    rods$ = new BehaviorSubject<number[][]>([
-      [3, 2, 1],
-      [],
-      []
-    ]);
+  public get Numbers() {
+    return numbers$
+  }
 
-    moves$ = rods$.pipe(
-      scan((state, rods) => {
+  public get CurrentItem() {
+    return currentItem$;
+  }
+
+  public get delayedObservable() {
+    return _delayedObservable;
+  }
 
   
-        
-        const fromIndex = rods.findIndex(rod => rod.length > 0);
-        const toIndex = rods.findIndex(rod => rod.length === 0 || rod[rod.length - 1] > rods[fromIndex][rods[fromIndex].length - 1]);
-
-        if (fromIndex !== -1 && toIndex !== -1) {
-          const disk = rods[fromIndex].pop()!;
-          rods[toIndex].push(disk);
-
-          console.log('hanoi disk : ' + rods)
-        }
-
-        return rods;
-      }, rods$.value)
-    );
-
-    moves$.subscribe({
-      next(value) { console.log('Hanoi next value: ' + value); },
-      error(err)  { console.error('Hanoi emitted an error: ' + err); },
-      complete()  { console.log('Hanoi complete notification'); }
-    });
-    */
-  }
-  //
-  public get CurrentValue()
-  {
-     return this._currentValue
-  }
-
-  public get Numbers()
-  {
-      return this.numbers$
-  }
-
-  public get CurrentItem()
-  {
-      return this.currentItem$;
+  public get delayedObservableHanoi() {
+    return _delayedObservableHanoi;
   }
 }
