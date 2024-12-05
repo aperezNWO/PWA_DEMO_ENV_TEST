9c4923b4a2027f44f8ada02d852465c748d74704
aperezNWO <alejandro.perez.acosta@hotmail.com>
2024-12-04 20:40:49 -0500

    [PWA DEMO] -- [ENV_TEST] v[1.3.27] 1) Speech to Text.

--- src/app/_modules/__Testing/speech-recognition/speech-recognition.component.ts
+++ src/app/_modules/__Testing/speech-recognition/speech-recognition.component.ts
@@ -1,24 +1,70 @@
 
-import { Component                } from '@angular/core';
-import { SpeechRecognitionService } from 'src/app/_services/speechRecognition/speech-recognition.service';
+import { Component, OnInit                } from '@angular/core';
 
 @Component({
   selector: 'app-speech-recognition',
   templateUrl: './speech-recognition.component.html',
   styleUrl: './speech-recognition.component.css'
 })
-export class SpeechRecognitionComponent  {
-    //
-    public transcript: string = '';
+export class SpeechRecognitionComponent  implements OnInit  {
+  recognition: any;
+  isListening = false;
+  transcript: string = '';
+  error: string = '';
 
-    constructor(public speechToTextService : SpeechRecognitionService ) {}
+  constructor() {
+    // Initialize the SpeechRecognition object
+    const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+    if (SpeechRecognition) {
+      this.recognition = new SpeechRecognition();
+      this.recognition.lang = 'es-CO'; // Set language
+      this.recognition.interimResults = false; // Only final results
+      this.recognition.maxAlternatives = 1;
 
-    startRecognition() {
-      this.speechToTextService.startRecognition();
+      // Event handlers
+      this.recognition.onresult = (event: any) => {
+        this.transcript = event.results[0][0].transcript;
+        console.log('Transcript:', this.transcript);
+      };
+
+      this.recognition.onerror = (event: any) => {
+        this.error = event.error;
+        console.error('Error:', this.error);
+      };
+
+      this.recognition.onend = () => {
+        this.isListening = false;
+        console.log('Recognition ended.');
+      };
+    } else {
+      alert('Speech Recognition API is not supported in your browser.');
     }
+  }
 
-    stopRecognition() {
-      this.speechToTextService.stopRecognition();
+  ngOnInit(): void {}
+
+  startListening() {
+    if (this.recognition) {
+      this.isListening = true;
+      this.recognition.start();
     }
+  }
+
+  stopListening() {
+    if (this.recognition) {
+      this.isListening = false;
+      this.recognition.stop();
+    }
+  }
+
+  speakText() {
+    if (this.transcript) {
+      const utterance = new SpeechSynthesisUtterance(this.transcript);
+      utterance.lang = 'es-CO';
+      window.speechSynthesis.speak(utterance);
+    } else {
+      alert('No text to speak!');
+    }
+  }
 }
 
