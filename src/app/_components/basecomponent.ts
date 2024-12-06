26f1225717c76537953fda7a3bf4ce7b9295ca86
aperezNWO <alejandro.perez.acosta@hotmail.com>
2024-12-05 21:55:42 -0500

    [PWA DEMO] -- [ENV_TEST] v[1.3.29] 1) grid component.

---
+++ src/app/_components/basecomponent.ts
@@ -0,0 +1,62 @@
+import { Inject, Injectable, QueryList, ViewChildren } from "@angular/core";
+import { Observable, of                              } from "rxjs";
+//import { _BaseModel, SiteRole                        } from "../_models/common/common";
+//import { _environment                                } from "../../environments/environment";
+//import { ConfigService                               } from "../_services/_config/config.service";
+//import { BaseService                                 } from "../_services/_config/base.service";
+//import { AuthService                                 } from "../_services/_config/auth.service";
+//import { _BaseSortEvent, BaseSortableHeader          } from "../_directives/BaseSortableHeader.directive";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class BaseComponent
+{
+    //
+    //@ViewChildren(BaseSortableHeader) headers: QueryList<BaseSortableHeader> | undefined;
+    //
+	//public PagesList!       : Observable<_BaseModel[]>;
+	public total!           : Observable<number>; 
+	//
+	//public ConfigRoleString : string = SiteRole.RoleConfig.toString(); 
+    //
+    constructor( /*public __service       : BaseService,*/
+                 /*ublic __authService   : AuthService,*/
+                 /*public __configService : ConfigService,*/
+                 @Inject('dictionaryKey') public _dictionaryKey: string,
+               )
+    {
+        //
+        //this.PagesList       = of([]);
+        //
+        //const pageSetting    = _environment.pageSettingDictionary[_dictionaryKey];
+        //
+        let _environmentList : string[] = [];
+        /*
+        __configService.loadJsonData(pageSetting.p_Path,
+                                    _environmentList).then(() => {
+            //
+            this.__service._SEARCH_PAGES.splice(0,this.__service._SEARCH_PAGES.length);
+            //
+            _environmentList.forEach((element: any) => {
+                this.__service._SEARCH_PAGES.push(element);
+            });
+            //    
+            this.PagesList  = __service.Pagelist;
+            this.total      = __service.total;
+        });*/
+    }
+    //
+    /*
+    onSort({ _column, _direction }: _BaseSortEvent) {
+        // resetting other headers
+        this.headers?.forEach((header) => {
+            if (header.sortable !== _column) {
+                header.direction= '';
+            }
+        });
+        //
+        //this.__service.sortColumn    = _column;
+        //this.__service.sortDirection = _direction;
+    }*/
+}
